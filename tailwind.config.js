9c197218f96e29bd6e08f7ee0608361373ef6577
erme07 <erik.medina07@gmail.com>
2024-10-22 20:37:11 -0300

    start

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'clicker' : ['"Clicker Script"', 'sans-serif'],
+        'work' : ['"Work Sans"', 'san-serif'],
+      },
+      colors: {
+        'back': '#F6F4ED',
+        'primary': '#FDC400',
+        'secondary': '#7B29DD'
+      },
+    },
+    container: {
+      center: true, // Centra el contenedor
+      padding: {
+        DEFAULT: '10px',
+        lg: '0px'
+      },
+      screens: {
+          lg: '992px',
+          xl: '1180px'
+      },
+    },
+  },
+  plugins: [],
+}
+
