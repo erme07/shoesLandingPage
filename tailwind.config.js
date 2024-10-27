8e86e455d5a022f7488ee8fdcd08ed7a67ae1e46
erme07 <erik.medina07@gmail.com>
2024-10-26 22:15:54 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,15 @@ module.exports = {
         'work' : ['"Work Sans"', 'san-serif'],
         'clash' : ['"Clash Display"', 'san-serif'],
       },
+      fontSize: {
+        '23':'1.438rem',
+        '26':'1.625rem',
+        'responsive-1': 'clamp(3.125rem, 2.2321rem + 4.4643vw, 4.375rem)',
+        'responsive-2': 'clamp(2.25rem, 1.8036rem + 2.2321vw, 2.875rem)',
+        'responsive-3': 'clamp(3.25rem, 2.8036rem + 2.2321vw, 3.875rem)',
+      },
       colors: {
+        'dark':'#1E1E1E',
         'back': '#F6F4ED',
         'primary': '#FDC400',
         'secondary': '#7B29DD'
@@ -28,8 +36,9 @@ module.exports = {
     container: {
       center: true,
       padding: {
-        DEFAULT: '20px',
-        md: '15px'
+        DEFAULT: '10px',
+        md: '15px',
+        lg: '20px'
       },
       screens: {
         'sm': '100%',
